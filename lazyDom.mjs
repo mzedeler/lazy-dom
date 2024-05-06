737da2d8cacdc03a8ce52c03f00d9e78f2def171
michael@zedeler.dk <michael@zedeler.dk>
2024-05-06 21:07:55 +0200

    Now with query support.

--- lazyDom.mjs
+++ lazyDom.mjs
@@ -1,10 +1,28 @@
+
+class Element {
+  get outerHTML() {
+    return ''
+  }
+  matches() {
+    return false
+  }
+  get childNodes() {
+    return []
+  }
+  querySelector(x) {
+    return new Element()
+  }
+  querySelectorAll(x) {
+    return []
+  }
+}
+
+class Document extends Element {
+  body = new Element()
+}
+
 const lazyDom = () => {
-  globalThis.document = {
-    body: {}
-  }
-  global.document = {
-    body: {}
-  }
+  global.document = new Document()
 }
 
 export default lazyDom
--- lazyDom.test.mjs
+++ lazyDom.test.mjs
@@ -6,6 +6,13 @@ describe('main', () => {
   it('initializes correctly', async () => {
     lazyDom()
 
-    expect(document).to.exist;
+    expect(document).to.exist
+  })
+
+  it('supports screen.queryByText() with empty DOM', async () => {
+    lazyDom()
+    const { screen } = await import('@testing-library/dom')
+
+    expect(screen.queryByText('hello')).to.be.null
   })
 })
