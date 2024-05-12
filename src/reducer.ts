103430b3975606105a18f966b3e84d63257e77e2
michael@zedeler.dk <michael@zedeler.dk>
2024-05-12 21:32:02 +0200

    Better watch mode.

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,17 +1,36 @@
-const initialState = {
+type State = {
+  objects: Object[],
+  values: WeakMap<Object, Record<string, unknown>>
+}
+
+const initialState: State = {
   objects: [],
   values: new WeakMap()
 }
 
-export const reducer = (state = initialState, action) => {
-  switch (action?.type) {
+type AddCommand = {
+  type: 'addObject',
+  object: Object
+}
+
+type SetCommand = {
+  type: 'setValue'
+  object: Object,
+  property: string
+  value: unknown
+}
+
+type Command = AddCommand | SetCommand
+
+export const reducer = (state: State = initialState, command?: Command) => {
+  switch (command?.type) {
     case 'addObject':
-      state.objects.push(action.object)
+      state.objects.push(command.object)
       return state
     case 'setValue':
-      const values = state.values.get(action.object) || {}
-      values[action.property] = action.value
-      state.values.set(action.object, values)
+      const values = state.values.get(command.object) || {}
+      values[command.property] = command.value
+      state.values.set(command.object, values)
       return state
   }
 
