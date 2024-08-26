d4c0465ae3201678dd2cfa99c0b0a559b81e240d
michael@zedeler.dk <michael@zedeler.dk>
2024-08-26 21:16:12 +0200

    Reenabled various tests.

--- test/testing-library-react.test.ts
+++ test/testing-library-react.test.ts
@@ -16,7 +16,7 @@ describe('@testing-library/react', () => {
     expect(screen.queryByText('hello3')).not.to.be.null
   })
 
-  xdescribe('screen.debug()', () => {
+  describe('screen.debug()', () => {
     let stub: SinonStub
 
     beforeEach(() => {
@@ -37,25 +37,25 @@ describe('@testing-library/react', () => {
     })
    })
  
-  xit('supports act', async () => {
-    // let callback:
-    // const subscribe = (cb) => { callback = cb }
-    // const Component = () => {
-    //   const count = useSyncExternalStore(subscribe, getSnapshot)
-    //   return React.createElement('input', { value, onChange: setValue })
-    // }
+  // it('supports act', async () => {
+  //   let callback:
+  //   const subscribe = (cb) => { callback = cb }
+  //   const Component = () => {
+  //     const count = useSyncExternalStore(subscribe, getSnapshot)
+  //     return React.createElement('input', { value, onChange: setValue })
+  //   }
 
-    // const { container } = render(React.createElement(Component))
+  //   const { container } = render(React.createElement(Component))
     
-    // console.log(screen.debug(container))
-  })
-
-  // it('supports screen.queryByText() where it finds a node', async () => {
-  //   const div = document.createElement('div')
-  //   const textNode = document.createTextNode('hello')
-  //   div.appendChild(textNode)
-  //   document.body.appendChild(div)
-
-  //   expect(screen.queryByText('hello')).to.eq(div)
+  //   console.log(screen.debug(container))
   // })
+
+  it('supports screen.queryByText() where it finds a node', async () => {
+    const div = document.createElement('div')
+    const textNode = document.createTextNode('hello')
+    div.appendChild(textNode)
+    document.body.appendChild(div)
+
+    expect(screen.queryByText('hello')).to.eq(div)
+  })
 })
