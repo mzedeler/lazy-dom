a3aa065bbb88e9de3434c74415baf66b2c898cdf
michael@zedeler.dk <michael@zedeler.dk>
2025-03-30 22:55:50 +0200

    Added more tests.

--- test/testing-library-dom.test.ts
+++ test/testing-library-dom.test.ts
@@ -37,4 +37,32 @@ describe('@testing-library/dom', () => {
 
     expect(screen.queryByTestId('some-id')).to.eq(div)
   })
+
+  it('supports screen.queryByTitle', () => {
+    const title = 'some-title'
+    const div = document.createElement('div')
+    div.setAttribute('title', title)
+    document.body.appendChild(div)
+
+    expect(screen.queryByTitle(title)).to.eq(div)
+  })
+
+  it('supports screen.queryByAltText', () => {
+    const altText = 'some-text'
+    const img = document.createElement('img')
+    img.setAttribute('alt', altText)
+    document.body.appendChild(img)
+
+    expect(screen.queryByAltText(altText)).to.eq(img)
+  })
+
+  it('supports screen.queryByText', () => {
+    const text = 'hello there'
+    const div = document.createElement('div')
+    const textNode = document.createTextNode(text)
+    div.appendChild(textNode)
+    document.body.appendChild(div)
+
+    expect(screen.queryByText(text)).to.eq(div)
+  })
 })
